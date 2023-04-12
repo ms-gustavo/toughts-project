cb14fbfaab4039208f4d0d0cfc93f04c79ef14f3
Gustavo Maia <ms.gustavo@outlook.com>
2023-04-12 12:54:01 -0300

    criaçao da home do projeto

---
+++ controllers/ToughtController.js
@@ -0,0 +1,9 @@
+const { response } = require("express");
+const Tought = require("../models/Tought");
+const User = require("../models/User");
+
+module.exports = class ToughtController {
+  static async showToughts(req, res) {
+    res.render("toughts/home");
+  }
+};
--- index.js
+++ index.js
@@ -12,6 +12,12 @@ const conn = require("./db/conn");
 const Tought = require("./models/Tought");
 const User = require("./models/User");
 
+// Import Routes
+const toughtsRoutes = require("./routes/toughtsRoutes");
+
+// Import Controller
+const ToughtController = require("./controllers/ToughtController");
+
 //template engine
 app.engine("handlebars", exphbs.engine());
 app.set("view engine", "handlebars");
@@ -58,7 +64,12 @@ app.use((req, res, next) => {
   next();
 });
 
+// Routes
+app.use("/toughts", toughtsRoutes);
+app.get("/", ToughtController.showToughts);
+
 conn
+  // resetar tabelas .sync({force : true})
   .sync()
   .then(() => {
     app.listen(3000);
